529460a4dfcd6f32e7d4bd5a17140baf449fd281
André Paulino <andrepaulinobtw@gmail.com>
2023-10-12 20:36:47 -0300

    feat: create lists tasks endpoint

--- src/main/java/com/andrepaulino/todolist/task/TaskController.java
+++ src/main/java/com/andrepaulino/todolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package com.andrepaulino.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,4 +43,11 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        UUID userId = (UUID) request.getAttribute("userId");
+        List<TaskModel> tasks = this.taskRepository.findByUserId(userId);
+
+        return tasks;
+    }
 }
