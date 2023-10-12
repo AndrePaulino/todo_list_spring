f421f6058b38d7106ee02ec59d2617878a8574cd
André Paulino <andrepaulinobtw@gmail.com>
2023-10-12 20:08:22 -0300

    feat: Add date validation

--- src/main/java/com/andrepaulino/todolist/task/TaskController.java
+++ src/main/java/com/andrepaulino/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package com.andrepaulino.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,24 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         UUID userId = (UUID) request.getAttribute("userId");
         taskModel.setUserId(userId);
+
+        LocalDateTime curentDate = LocalDateTime.now();
+        if (curentDate.isAfter(taskModel.getStartAt()) || curentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Data de início/término deve ser depois da atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Data de término deve ser após a de início");
+        }
+
         TaskModel task = this.taskRepository.save(taskModel);
-        return task;
+
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
 }
